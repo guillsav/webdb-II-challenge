fec35607c5faf6c18d79bd62188a5721c2167d60
guillsav <guillsav@gmail.com>
2019-04-30 16:31:50 -0400

    Stretch done

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const server = require('./api/server');
 
-// endpoints here
-
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
