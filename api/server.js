d0a0ee9422b022b7241f0b43a0706f59406d9545
guillsav <guillsav@gmail.com>
2019-04-30 15:52:37 -0400

    changed folder structure

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const zoosRouter = require('./zoos/zoos-router.js');
+const zoosRouter = require('../zoos/zoos-router.js');
 
 const server = express();
 
